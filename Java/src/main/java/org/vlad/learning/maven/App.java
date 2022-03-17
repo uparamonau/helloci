44b19bc3a6358580d6c754d25ff8d281624424e7
Uladzimir Paramonau <uladzimir.paramonau@elastic.co>
2022-03-17 11:34:47 +0300

    commit sample Java Maven project

---
+++ Java/src/main/java/org/vlad/learning/maven/App.java
@@ -0,0 +1,13 @@
+package org.vlad.learning.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
